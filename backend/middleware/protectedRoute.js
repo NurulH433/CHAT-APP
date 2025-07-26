7c4deb38d14b3d07304232d4e3b7e7490767845d
NurulH123 <hidayatnurul937@gmail.com>
2025-07-27 06:21:57 +0700

    add middleware protected route

---
+++ backend/middleware/protectedRoute.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user.model.js";
+
+const protectedRoutes = async (req, res, next) => {
+
+    try {
+
+        const token = req.cookies.token;
+
+        if (!token) {
+            return res.status(400).json({
+                error: 'Unauthorized - No Token Provided'
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        
+        if (!decoded) {
+            return res.status(400).json({
+                error: 'Unauthorized - Invalid Token'
+            })
+        }
+
+        const user = await User.findById(decoded.userId).select('password')
+
+        if (!user) {
+            return res.status(400).json({
+                error: 'User not found'
+            })
+        }
+        
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        res.status(400).json({
+            message: 'pretected route internal error',
+            error: error.message
+        })
+    }
+
+}
+
+export default protectedRoutes
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { sendMessage } from "../controllers/messageController.js";
+import protectedRoutes from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
-router.post('/send/:userId', sendMessage)
+router.post('/send/:userId', protectedRoutes, sendMessage)
 
 export default router;
