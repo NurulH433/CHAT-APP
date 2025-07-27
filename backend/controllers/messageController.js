621193d08c5f6096233c3ff469bd6bc3bb46631d
NurulH123 <hidayatnurul937@gmail.com>
2025-07-27 08:59:30 +0700

    api controller mengirim pesan berhasil

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,6 +1,30 @@
-export const sendMessage = (req, res) => {
-    const {userId} = req.params;
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
-    console.log('message sent');
-    res.send(req.params);
+export const sendMessage = async (req, res) => {
+    const {message} = req.body;
+    const {id: receiverId} = req.params
+    const userId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+        participants: {$all: [userId, receiverId]}
+    })
+
+    if (!conversation) {
+        const conversation = await Conversation({
+            participants: [userId, receiverId]
+        })
+    }
+
+    const newMessage = new Message({
+        userId,
+        receiverId,
+        message
+    })
+
+    if (newMessage) {
+        conversation.messages.push(newMessage._id)
+    }
+
+    res.status(201).json(newMessage)
 }
